214f5f679eb52031e87e4a305ef6cf0366586861
Michael Wales <martijn.diks@gmail.com>
2018-07-24 14:11:01 +0200

    correct coneversion try

--- libs/GrowattModule.php
+++ libs/GrowattModule.php
@@ -135,7 +135,7 @@ class Growatt extends IPSModule
                 continue;
             }
             $this->SendDebug($Variable['Name'], $Value, 0);
-			$Value = $Value / 10;
+			//$Value = $Value / 10;
             $this->SetValueExt($Variable, $Value);
         }
         return true;
